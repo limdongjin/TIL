438c7f98ff3aa482f3fecf9e8345eb3503ebf69d
limdongjin <geniuslim27@gmail.com>
2018-12-14 23:32:18 +0900

    GA

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,29 +1,20 @@
 module.exports = {
     title: 'limdongjin',
     description: '도큐멘팅!',
-    head: [
-        ['link', {rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.1/css/bulma.min.css'}]
-    ],
     themeConfig: {
         nav: [
             { text: 'Home', link: '/' },
-            { text: 'Contact',
-                items: [
-                    { text: 'Github', link: 'https://github.com/limdongjin' },
-                    { text: 'Facebook', link: 'https://www.facebook.com/geniuslim27' },
-                    { text: 'LinkedIn', link: 'https://www.linkedin.com/in/dongjin-lim-98115a137/' }
-                ]
-            }
+            { text: 'Github', link: 'https://github.com/limdongjin' },
+            { text: 'Facebook', link: 'https://www.facebook.com/geniuslim27' },
+            { text: 'LinkedIn', link: 'https://www.linkedin.com/in/dongjin-lim-98115a137/' }
         ],
         sidebar: "auto",
-        lastUpdated: 'Last Updated',
-        serviceWorker: {
-            updatePopup: true // Boolean | Object, default to undefined.
-        }
+        lastUpdated: 'Last Updated'
     },
     plugins: [
         '@vuepress/blog',
-        'tag'
+        '@vuepress/pwa',
+        '@vuepress/google-analytics'
     ],
     configureWebpack: {
         resolve: {
--- README.md
+++ README.md
@@ -1,15 +1,6 @@
 ---
 home: true
 footer: MIT Licensed | Copyright © 2018-present Dongjin Lim
-actionText: Get Started →
-actionLink: /guide/
-features:
-- title: Simplicity First
-  details: Minimal setup with markdown-centered project structure helps you focus on writing.
-- title: Vue-Powered
-  details: Enjoy the dev experience of Vue + webpack, use Vue components in markdown, and develop custom themes with Vue.
-- title: Performant
-  details: VuePress generates pre-rendered static HTML for each page, and runs as an SPA once a page is loaded.
 heroText: 초고수를 향하여 
 ---
 # Today I Learned
