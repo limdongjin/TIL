7e360634bd387d654712d5e3c5b9426a81c1e258
limdongjin <geniuslim27@gmail.com>
2019-01-05 10:51:05 +0900

    vuepress Layout padding 조절

--- .vuepress/theme/Layout.vue
+++ .vuepress/theme/Layout.vue
@@ -1,7 +1,7 @@
 <template>
     <ParentLayout >
-       <TagLinks slot="page-bottom" v-if="this.$frontmatter.tags" class="content"/>
-        <Disqus slot="page-bottom" v-if="this.$frontmatter.tags" class="content"/>
+        <TagLinks slot="page-bottom" v-if="this.$frontmatter.tags" class="content padding_bottom_zero"/>
+        <Disqus slot="page-bottom" v-if="this.$frontmatter.tags" class="content padding_top_zero"/>
     </ParentLayout>
 </template>
 
@@ -17,3 +17,12 @@
         }
     }
 </script>
+
+<style>
+    .padding_top_zero {
+        padding-top: 0 !important;
+    }
+    .padding_bottom_zero {
+        padding-bottom: 0 !important;
+    }
+</style>
